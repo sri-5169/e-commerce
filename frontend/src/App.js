e1e15353443062d6cbf1aa1a8af1291e4353f8f4
Srinath Pandey <72240749+sri-5169@users.noreply.github.com>
2021-06-29 17:53:49 +0530

    Installed react Completed Home screen and Product screeen
    Now the front end has almost been completed and after this we will start the  introduction of backend.

---
+++ frontend/src/App.js
@@ -0,0 +1,33 @@
+
+import React from 'react';
+
+import {BrowserRouter , Route , LInk} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="grid-container">
+    <header className="row">
+      <div>
+        <a className="brand" href="/index">Kit-To-Kart</a>
+      </div>
+      <div>
+        <a href="/cart">Cart</a>
+        <a href="/signin">Sign In</a>
+      </div>
+    </header>
+    <main>
+      <Route path="/product/:id" component={ProductScreen}></Route>
+      <Route path="/" component={HomeScreen} exact></Route>
+      
+
+    </main>
+    <footer className="row center">All rights reserved</footer>
+  </div>
+  </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products:[
+        {
+        _id : '1',
+        name: 'Lenovo Thinkpad',
+        category: 'Laptop',
+        image: '/image/p1.jpg',
+        price: 120,
+        brand: 'Lenovo',
+        rating: 4.5,
+        numReviews :10,
+        description: '[High speed laptop'
+    },
+    {
+        _id : '2',
+        name: 'Apple BlueBook',
+        category: 'Laptop',
+        image: '/image/p2.jpg',
+        price: 119,
+        brand: 'Apple',
+        rating: 4.4,
+        numReviews :11,
+        description: '[High speed laptop'
+    },
+  {
+        _id : '3',
+        name: 'HP pavilion',
+        category: 'Laptop',
+        image: '/image/p3.jpg',
+        price: 117,
+        brand: 'HP',
+        rating: 4.3,
+        numReviews: 13,
+        description: '[High speed laptop'
+    },
+    {
+        _id : '4',
+        name: 'Dell Inspiron',
+        category : 'Laptop',
+        image: '/image/p4.jpg',
+        price: 125,
+        brand: 'Dell',
+        rating: 4.4,
+        numReviews :12,
+        description: '[High speed laptop'
+    },
+    {
+        _id : '5',
+        name: 'Microsoft Masto',
+        category: 'Laptop',
+        image: '/image/p5.jpg',
+        price: 123,
+        brand: 'Microsoft',
+        rating: 4.5,
+        numReviews :11,
+        description: '[High speed laptop'
+    },
+    {
+        _id : '6',
+        name: 'Acer extensa',
+        category: 'Laptop',
+        image: '/image/p6.jpg',
+        price: 121,
+        brand: 'Acer',
+        rating: 4.5,
+        numReviews :12,
+        description: '[High speed laptop'
+    }
+    ]
+};
+    
+export default data;
